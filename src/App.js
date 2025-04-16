a224974d36f2dd403639d974698cefae8c827b27
Zoha Mobin <zoha.mobin@securiti.ai>
2025-04-16 09:38:20 +0500

    Removed dashboard and attendance

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/global.css"; // Single global CSS file
 import Signup from "./Pages/LoginSignUp/Signup";
 import MainLayout from "./Pages/MainLayout/MainLayout";
-
-import Dashboard from "./Pages/Dashboard/Dashboard.jsx";
 import ClassSchedule from "./Pages/Class Schedule/Class Schedule.jsx";
 import ImportStudentsPage from "./Pages/ImportStudents/ImportStudentsPage";
 import StudentDirectoryPage from "./Pages/StudentDirectory/StudentDirectoryPage";
-
-import Attendance from "./Pages/Attendance/Attendance";
 import CoursePage from "./Pages/Course/CoursePage.js";
 import AssignSectionPage from "./Pages/AssignSection/AssignSectionPage.js";
 import CourseRegistrationPage from "./Pages/CourseRegistration/CourseRegistrationPage";
@@ -23,9 +19,7 @@ function App() {
           <Route path="/" element={<Signup />} />
 
           <Route path="/*" element={<MainLayout />}>
-            <Route path="dashboard" element={<Dashboard />} />
             <Route path="class-schedule" element={<ClassSchedule />} />
-            <Route path="Attendance" element={<Attendance />} />
             <Route path="Course" element={<CoursePage />} />
             <Route path="course-registration" element={<CourseRegistrationPage />} />
             <Route path="import-students" element={<ImportStudentsPage />} />
